72fbf212b00082fcf362c89b40b5601b31e76827
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-06-28 18:09:53 +0200

    Added vector methods skeleton

--- vector.h
+++ vector.h
@@ -17,14 +17,48 @@
         type * content;             \
     }
 
-#define vector(type)        \
-vector_named(type, type)
+#define vector(type) vector_named(type, type)
 
-#define typedef_vector_named(type, name) \
-typedef vector_named(type, name) vector_##name##_t
+#define vector_methods(type, name)                                                                  \
+static inline void vector_##name##_push_back(struct vector_##name * v, type value) {                \
+                                                                                                    \
+}                                                                                                   \
+                                                                                                    \
+static inline type vector_##name##_pop_back(struct vector_##name * v) {                             \
+    return (type) NULL;                                                                             \
+}                                                                                                   \
+                                                                                                    \
+static inline void vector_##name##_insert(struct vector_##name * v, type value, size_t position) {  \
+                                                                                                    \
+}                                                                                                   \
+                                                                                                    \
+static inline type vector_##name##_remove(struct vector_##name * v, size_t position) {              \
+    return (type) NULL;                                                                             \
+}                                                                                                   \
+                                                                                                    \
+static inline size_t vector_##name##_size(struct vector_##name * v) {                               \
+    return 0;                                                                                       \
+}                                                                                                   \
+                                                                                                    \
+static inline size_t vector_##name##_capacity(struct vector_##name * v) {                           \
+    return 0;                                                                                       \
+}                                                                                                   \
+                                                                                                    \
+static inline type * vector_##name##_data(struct vector_##name * v) {                               \
+    return NULL;                                                                                    \
+}                                                                                                   \
+                                                                                                    \
+static inline void vector_##name##_reserve(struct vector_##name * v) {                              \
+                                                                                                    \
+}                                                                                                   \
 
-#define typedef_vector(type) \
-typedef_vector_named(type, type)
+
+#define typedef_vector_named(type, name)        \
+vector_named(type, name);                       \
+vector_methods(type, name)                      \
+typedef struct vector_##name vector_##name##_t
+
+#define typedef_vector(type) typedef_vector_named(type, type)
 
 #define vector_create_with_capacity(type, cap)                      \
     {                                                               \
