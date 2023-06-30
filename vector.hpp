39b4081d526185ac7c266f0100477958abe183de
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-06-30 17:41:52 +0200

    Added `inline` and `constexpr` in the C++ wrapper where possible

--- vector.hpp
+++ vector.hpp
@@ -30,98 +30,98 @@
 
  #include <vector>
 
- #define vector_methods_cxx(type, name)                                      \
- namespace dc4c {                                                            \
- class vector_bridge_##name {                                                \
-     vector_##name underlying;                                               \
-                                                                             \
- public:                                                                     \
-     vector_bridge_##name() {                                                \
-         vector_##name##_create(&underlying);                                \
-     }                                                                       \
-                                                                             \
-     vector_bridge_##name(const vector_bridge_##name & other) {              \
-         vector_##name##_copy(&underlying, &other.underlying);               \
-     }                                                                       \
-                                                                             \
-     vector_bridge_##name(vector_bridge_##name && other) {                   \
-         vector_##name##_destroy(&underlying);                               \
-         underlying = other.underlying;                                      \
-         other.underlying = {};                                              \
-     }                                                                       \
-                                                                             \
-     vector_bridge_##name(const std::vector<type> & other) {                 \
-         vector_##name##_create(&underlying);                                \
-         vector_##name##_reserve(&underlying, other.capacity());             \
-                                                                             \
-         for (const auto & element : other) {                                \
-             vector_##name##_push_back(&underlying, element);                \
-         }                                                                   \
-     }                                                                       \
-                                                                             \
-     ~vector_bridge_##name() {                                               \
-         vector_##name##_destroy(&underlying);                               \
-     }                                                                       \
-                                                                             \
-     vector_bridge_##name & operator=(const vector_bridge_##name & other) {  \
-         vector_##name##_destroy(&underlying);                               \
-         vector_##name##_copy(&underlying, &other.underlying);               \
-         return *this;                                                       \
-     }                                                                       \
-                                                                             \
-     vector_bridge_##name & operator=(vector_bridge_##name && other) {       \
-         vector_##name##_destroy(&underlying);                               \
-         underlying = other.underlying;                                      \
-         other.underlying = {};                                              \
-         return *this;                                                       \
-     }                                                                       \
-                                                                             \
-     vector_##name & data() {                                                \
-         return underlying;                                                  \
-     }                                                                       \
-                                                                             \
-     const vector_##name & data() const {                                    \
-         return underlying;                                                  \
-     }                                                                       \
-                                                                             \
-     operator vector_##name *() {                                            \
-         return &underlying;                                                 \
-     }                                                                       \
-                                                                             \
-     operator const vector_##name *() const {                                \
-         return &underlying;                                                 \
-     }                                                                       \
-                                                                             \
-     inline operator std::vector<type>() const {                             \
-         auto toReturn = std::vector<type>();                                \
-         toReturn.reserve(underlying.cap);                                   \
-         for (size_t i = 0; i < underlying.count; ++i) {                     \
-             toReturn.push_back(underlying.content[i]);                      \
-         }                                                                   \
-         return toReturn;                                                    \
-     }                                                                       \
-                                                                             \
-     vector_##name * operator->() {                                          \
-         return *this;                                                       \
-     }                                                                       \
-                                                                             \
-     const vector_##name * operator->() const {                              \
-         return *this;                                                       \
-     }                                                                       \
-                                                                             \
-     type & operator[](const size_t position) const noexcept {               \
-         return underlying.content[position];                                \
-     }                                                                       \
-                                                                             \
-     type * begin() const noexcept {                                         \
-         return underlying.content;                                          \
-     }                                                                       \
-                                                                             \
-     type * end() const noexcept {                                           \
-         return underlying.content + underlying.count;                       \
-     }                                                                       \
- };                                                                          \
- }                                                                           \
+ #define vector_methods_cxx(type, name)                                                    \
+ namespace dc4c {                                                                          \
+ class vector_bridge_##name {                                                              \
+     vector_##name underlying;                                                             \
+                                                                                           \
+ public:                                                                                   \
+     inline vector_bridge_##name() {                                                       \
+         vector_##name##_create(&underlying);                                              \
+     }                                                                                     \
+                                                                                           \
+     inline vector_bridge_##name(const vector_bridge_##name & other) {                     \
+         vector_##name##_copy(&underlying, &other.underlying);                             \
+     }                                                                                     \
+                                                                                           \
+     inline vector_bridge_##name(vector_bridge_##name && other) {                          \
+         vector_##name##_destroy(&underlying);                                             \
+         underlying = other.underlying;                                                    \
+         other.underlying = {};                                                            \
+     }                                                                                     \
+                                                                                           \
+     inline vector_bridge_##name(const std::vector<type> & other) {                        \
+         vector_##name##_create(&underlying);                                              \
+         vector_##name##_reserve(&underlying, other.capacity());                           \
+                                                                                           \
+         for (const auto & element : other) {                                              \
+             vector_##name##_push_back(&underlying, element);                              \
+         }                                                                                 \
+     }                                                                                     \
+                                                                                           \
+     inline ~vector_bridge_##name() {                                                      \
+         vector_##name##_destroy(&underlying);                                             \
+     }                                                                                     \
+                                                                                           \
+     inline auto operator=(const vector_bridge_##name & other) -> vector_bridge_##name & { \
+         vector_##name##_destroy(&underlying);                                             \
+         vector_##name##_copy(&underlying, &other.underlying);                             \
+         return *this;                                                                     \
+     }                                                                                     \
+                                                                                           \
+     inline auto operator=(vector_bridge_##name && other) -> vector_bridge_##name & {      \
+         vector_##name##_destroy(&underlying);                                             \
+         underlying = other.underlying;                                                    \
+         other.underlying = {};                                                            \
+         return *this;                                                                     \
+     }                                                                                     \
+                                                                                           \
+     inline auto data() -> vector_##name & {                                               \
+         return underlying;                                                                \
+     }                                                                                     \
+                                                                                           \
+     constexpr auto data() const -> const vector_##name & {                                \
+         return underlying;                                                                \
+     }                                                                                     \
+                                                                                           \
+     inline operator vector_##name *() {                                                   \
+         return &underlying;                                                               \
+     }                                                                                     \
+                                                                                           \
+     constexpr inline operator const vector_##name *() const {                             \
+         return &underlying;                                                               \
+     }                                                                                     \
+                                                                                           \
+     inline operator std::vector<type>() const {                                           \
+         auto toReturn = std::vector<type>();                                              \
+         toReturn.reserve(underlying.cap);                                                 \
+         for (size_t i = 0; i < underlying.count; ++i) {                                   \
+             toReturn.push_back(underlying.content[i]);                                    \
+         }                                                                                 \
+         return toReturn;                                                                  \
+     }                                                                                     \
+                                                                                           \
+     inline auto operator->() -> vector_##name * {                                         \
+         return *this;                                                                     \
+     }                                                                                     \
+                                                                                           \
+     constexpr inline auto operator->() const -> const vector_##name * {                   \
+         return *this;                                                                     \
+     }                                                                                     \
+                                                                                           \
+     constexpr inline auto operator[](const size_t position) const noexcept -> type & {    \
+         return underlying.content[position];                                              \
+     }                                                                                     \
+                                                                                           \
+     constexpr inline auto begin() const noexcept -> type * {                              \
+         return underlying.content;                                                        \
+     }                                                                                     \
+                                                                                           \
+     constexpr inline auto end() const noexcept -> type * {                                \
+         return underlying.content + underlying.count;                                     \
+     }                                                                                     \
+ };                                                                                        \
+ }                                                                                         \
  
  
  #endif /* vector_hpp */
