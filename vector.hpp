287d86aacdedac5023e4f15efbe448ea53f3cc47
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-06-30 16:04:14 +0200

    Added inclusion guard to the C++ wrapper

--- vector.hpp
+++ vector.hpp
@@ -17,75 +17,80 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef vector_hpp
-#define vector_hpp
+#ifndef vector_h
+ #warning Wrong inclusion of "vector.hpp" redirected to #include "vector.h"!
+ #include "vector.h"
+#else
+ #ifndef vector_hpp
+ #define vector_hpp
 
-#define vector_methods_cxx(type, name)                                      \
-namespace dc4c {                                                            \
-class vector_bridge_##name {                                                \
-    vector_##name underlying;                                               \
-                                                                            \
-public:                                                                     \
-    vector_bridge_##name() {                                                \
-        vector_##name##_create(&underlying);                                \
-    }                                                                       \
-                                                                            \
-    vector_bridge_##name(const vector_bridge_##name & other) {              \
-        vector_##name##_copy(&underlying, &other.underlying);               \
-    }                                                                       \
-                                                                            \
-    vector_bridge_##name(vector_bridge_##name && other) {                   \
-        vector_##name##_destroy(&underlying);                               \
-        underlying = other.underlying;                                      \
-        other.underlying = {};                                              \
-    }                                                                       \
-                                                                            \
-    ~vector_bridge_##name() {                                               \
-        vector_##name##_destroy(&underlying);                               \
-    }                                                                       \
-                                                                            \
-    vector_bridge_##name & operator=(const vector_bridge_##name & other) {  \
-        vector_##name##_destroy(&underlying);                               \
-        vector_##name##_copy(&underlying, &other.underlying);               \
-        return *this;                                                       \
-    }                                                                       \
-                                                                            \
-    vector_bridge_##name & operator=(vector_bridge_##name && other) {       \
-        vector_##name##_destroy(&underlying);                               \
-        underlying = other.underlying;                                      \
-        other.underlying = {};                                              \
-        return *this;                                                       \
-    }                                                                       \
-                                                                            \
-    vector_##name & data() {                                                \
-        return underlying;                                                  \
-    }                                                                       \
-                                                                            \
-    const vector_##name & data() const {                                    \
-        return underlying;                                                  \
-    }                                                                       \
-                                                                            \
-    operator vector_##name *() {                                            \
-        return &underlying;                                                 \
-    }                                                                       \
-                                                                            \
-    operator const vector_##name *() const {                                \
-        return &underlying;                                                 \
-    }                                                                       \
-                                                                            \
-    vector_##name * operator->() {                                          \
-        return *this;                                                       \
-    }                                                                       \
-                                                                            \
-    const vector_##name * operator->() const {                              \
-        return *this;                                                       \
-    }                                                                       \
-                                                                            \
-    type & operator[](const size_t position) const noexcept {               \
-        return underlying.content[position];                                \
-    }                                                                       \
-};                                                                          \
-}                                                                           \
-
-
-#endif /* vector_hpp */
+ #define vector_methods_cxx(type, name)                                      \
+ namespace dc4c {                                                            \
+ class vector_bridge_##name {                                                \
+     vector_##name underlying;                                               \
+                                                                             \
+ public:                                                                     \
+     vector_bridge_##name() {                                                \
+         vector_##name##_create(&underlying);                                \
+     }                                                                       \
+                                                                             \
+     vector_bridge_##name(const vector_bridge_##name & other) {              \
+         vector_##name##_copy(&underlying, &other.underlying);               \
+     }                                                                       \
+                                                                             \
+     vector_bridge_##name(vector_bridge_##name && other) {                   \
+         vector_##name##_destroy(&underlying);                               \
+         underlying = other.underlying;                                      \
+         other.underlying = {};                                              \
+     }                                                                       \
+                                                                             \
+     ~vector_bridge_##name() {                                               \
+         vector_##name##_destroy(&underlying);                               \
+     }                                                                       \
+                                                                             \
+     vector_bridge_##name & operator=(const vector_bridge_##name & other) {  \
+         vector_##name##_destroy(&underlying);                               \
+         vector_##name##_copy(&underlying, &other.underlying);               \
+         return *this;                                                       \
+     }                                                                       \
+                                                                             \
+     vector_bridge_##name & operator=(vector_bridge_##name && other) {       \
+         vector_##name##_destroy(&underlying);                               \
+         underlying = other.underlying;                                      \
+         other.underlying = {};                                              \
+         return *this;                                                       \
+     }                                                                       \
+                                                                             \
+     vector_##name & data() {                                                \
+         return underlying;                                                  \
+     }                                                                       \
+                                                                             \
+     const vector_##name & data() const {                                    \
+         return underlying;                                                  \
+     }                                                                       \
+                                                                             \
+     operator vector_##name *() {                                            \
+         return &underlying;                                                 \
+     }                                                                       \
+                                                                             \
+     operator const vector_##name *() const {                                \
+         return &underlying;                                                 \
+     }                                                                       \
+                                                                             \
+     vector_##name * operator->() {                                          \
+         return *this;                                                       \
+     }                                                                       \
+                                                                             \
+     const vector_##name * operator->() const {                              \
+         return *this;                                                       \
+     }                                                                       \
+                                                                             \
+     type & operator[](const size_t position) const noexcept {               \
+         return underlying.content[position];                                \
+     }                                                                       \
+ };                                                                          \
+ }                                                                           \
+ 
+ 
+ #endif /* vector_hpp */
+#endif /* vector_h */
