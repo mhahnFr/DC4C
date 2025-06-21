e9a79e2d48839b769fc810ea4f058f651313bc0b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2025-06-21 17:08:57 +0200

    Fixed using `vector_create` instead of `vector_init`

--- DC4C/vector.hpp
+++ DC4C/vector.hpp
@@ -145,7 +145,7 @@ public:
     __DC4C_CONSTEXPR_SINCE_CXX14 inline auto operator=(vector&& other) noexcept -> vector& {
         vector_destroy(&underlying);
         underlying = other.underlying;
-        vector_create(&other.underlying);
+        vector_init(&other.underlying);
         return *this;
     }
 
